fcbe872525022f67c2ba2f9b4235230a338ba02d
ayahosnii <96696906+ayahosnii@users.noreply.github.com>
2023-06-20 20:25:35 +0300

    Array as a Data Structure - Part 1

--- array_1.cpp
+++ array_1.cpp
@@ -6,7 +6,7 @@ class Array
 private:
     int size;
     int length;
-    int* items;
+    int * items;
 
 public:
     Array(int arrsize)
@@ -37,10 +37,38 @@ public:
         }
         
     }
-}
+    void Display()
+    {
+         cout << "Display Array Content \n";
+         for (int i = 0; i < length; i++)
+         {
+            cout << items[i] << "\t";
+         }
+
+         cout << endl;
+    }
+
+    int getSize()
+    {
+        return size;
+    }
+
+    int getLength()
+    {
+        return length;
+    }
+};
 
 
 int main()
 {
-    count << "Hello This is Array ADT demoin";
-} 
+    cout << "Hello This is Array ADT demoin";
+    int arraysize;
+    cout << "Enter the Array Size \n";
+    cin >> arraysize;
+
+    Array myarray(arraysize);
+    myarray.Fill();
+    cout << "The Array Size = " << myarray.getSize() << " While the length = " << myarray.getLength() << "\n";
+    myarray.Display();
+}
