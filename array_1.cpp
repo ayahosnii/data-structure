9199698890330b6ed5df04b59fd4f5460d96e9aa
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-06-27 18:33:28 +0300

    Search, Insert etc

--- array_1.cpp
+++ array_1.cpp
@@ -9,12 +9,15 @@ private:
     int * items;
 
 public:
+    // Constructor to initialize the Array object
     Array(int arrsize)
     {
         size = arrsize;
         length = 0;
-        items = new int(arrsize);
+        items = new int[arrsize]; // Dynamically allocate memory for the array
     }
+
+    // Method to fill the array with user input
     void Fill()
     {
         int no_of_items;
@@ -33,10 +36,10 @@ public:
                 cin >> items[i];
                 length ++;
             }
-            
         }
-        
     }
+    
+    // Method to display the content of the array
     void Display()
     {
          cout << "Display Array Content \n";
@@ -44,31 +47,118 @@ public:
          {
             cout << items[i] << "\t";
          }
-
          cout << endl;
     }
 
+    // Method to get the size of the array
     int getSize()
     {
         return size;
     }
 
+    // Method to get the current length of the array
     int getLength()
     {
         return length;
+    }
+
+    // Method to search for a given key in the array
+      int Search(int key)
+      {
+        int index = -1;
+        for (int i = 0; i < length; i++)
+        {
+            if (items[i] == key)
+            {
+                index = i;
+                break;
+            }
+
+        }
+
+  return index;
+    }
+
+    // Method to append a new item to the end of the array
+    void Append(int newitem)
+    {
+        if (length < size)
+        {
+            items[length] = newitem;
+            length++;            
+        }
+        else
+        cout << "Array is full \n"; 
+    }
+
+    // Method to insert a new item at a specific index in the array
+    void Insert(int index, int newitem)
+    {
+        if (index >= 0 && index < size)
+        {
+            for (int i = length; i > index; i--)
+            {
+                items[i] = items[i - 1];
+            }
+            items[index] = newitem;
+            length++;
+        }
+        else
+        {
+            cout << "Error - index out of Range \n";
+        }
     }
 };
 
 
 int main()
 {
-    cout << "Hello This is Array ADT demoin";
+    cout << "Hello This is Array ADT demo \n";
     int arraysize;
     cout << "Enter the Array Size \n";
     cin >> arraysize;
 
-    Array myarray(arraysize);
-    myarray.Fill();
-    cout << "The Array Size = " << myarray.getSize() << " While the length = " << myarray.getLength() << "\n";
-    myarray.Display();
+    Array myarray(arraysize); // Create an Array object with the specified size
+
+    myarray.Fill(); // Fill the array with user input
+    
+    cout << "The Array Size = " <<
+             myarray.getSize() << 
+             " While the length = " << 
+             myarray.getLength() << "\n";
+
+
+    myarray.Display(); // Display the content of the array
+
+/*
+    cout << "Enter the value to search for \n";
+    int key;
+    cin >> key;
+    
+    int index =   myarray.Search(key);
+    if (index == -1)
+        cout << "Item not found \n";
+    else
+        cout << "Item found @ position " << index << endl;
+*/    
+    int newitem;
+    cout << "Enter a new item to add to the array \n";
+    cin >> newitem;
+    myarray.Append(newitem); // Append the new item to the array
+    myarray.Display(); // Display the updated array
+
+   
+    cout << "Enter an index and item to insert into the array";
+    cin >> index;
+    cin >> newitem;
+
+    myarray.Insert(index, newitem); // Insert the item at the specified index in the array
+    myarray.Display(); // Display the updated array
+
+    cout << "The Array Size = " <<
+             myarray.getSize() << 
+             " While the length = " << 
+             myarray.getLength() << "\n";
+
+    return 0;
 }
